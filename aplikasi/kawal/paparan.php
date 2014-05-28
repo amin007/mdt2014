83e86579f2c8f2c3c7ac468fc46f5526ae948085
Amin PO Muar <helmi007@yahoo.com>
2014-05-28 16:15:39 +0800

    ubausuai fungsi respon()

--- aplikasi/kawal/paparan.php
+++ aplikasi/kawal/paparan.php
@@ -6,6 +6,48 @@ class Paparan extends Kawal
     {
         parent::__construct();
         Kebenaran::kawalKeluar();
+
+        $this->papar->js = array(
+            //'bootstrap.js',
+            'bootstrap-transition.js',
+            'bootstrap-alert.js',
+            'bootstrap-modal.js',
+            'bootstrap-dropdown.js',
+            'bootstrap-scrollspy.js',
+            'bootstrap-tab.js',
+            'bootstrap-tooltip.js',
+            'bootstrap-popover.js',
+            'bootstrap-button.js',
+            'bootstrap-collapse.js',
+            'bootstrap-carousel.js',
+            'bootstrap-typeahead.js',
+            'bootstrap-affix.js',
+            'bootstrap-datepicker.js',
+            'bootstrap-datepicker.ms.js',
+            'bootstrap-editable.min.js');
+        $this->papar->css = array(
+            'bootstrap-datepicker.css',
+            'bootstrap-editable.css');
+
+			$this->medanRangka = 'newss,ssm,concat_ws("<br>",nama,operator) as nama,'
+			. 'fe,msic,sv,utama,respon R,' . "\r"
+			. 'concat_ws("<br>",alamat1,alamat2,poskod,ngdbbp) as alamat,' . "\r"
+			. 'thn,concat_ws("<br>",semak_rangka12,data12) as data12,label,' . "\r"
+			. 'tel,fax,concat_ws("<br>","PENGURUS|PEMILIK|KERANI",responden) as Orang,'
+			. 'email,nota,msic08';
+			//. 'newss,msic,nama,utama,fe,terima,'
+			//. 'hasil,dptLain,web,stok,staf,gaji,outlet,sebab';
+		$this->medanData = 'newss,msic,utama,nama,fe,terima,'
+			. ' format( (hasil + IFNULL(dptLain,0) ), 0 ) as dapat,'
+			//. '(hasil+COALESCE(dptLain,0)) as dapat2,'
+			. 'format(hasil,0) as hasil,format(dptlain,0) as dptlain,' . "\r"
+			. 'format(stok,0) as stok,staf,format(gaji,0) as gaji,' . "\r"
+			. 'outlet,sebab';
+		$this->sv = 'mdt_';
+		$this->_folder = 'kawalan';
+		$this->pengguna = Sesi::get('namaPegawai');
+		$this->level = Sesi::get('levelPegawai');
+		
     }
     
     public function index() 
@@ -32,8 +74,8 @@ class Paparan extends Kawal
 			case "tegar" 	: $kesRespon = 'kesTegar'; $break;
 			default 		: $kesRespon = 'paparSemua';
 		}
-		echo "$kesRespon | $item | $ms | $fe | $cetak <br>";
-		/*
+		//echo "$kesRespon | $item | $ms | $fe | $cetak <br>";
+		
 		$fe = ($this->level == 'kawal') ? $fe : $this->pengguna; # set nama fe
         $bulanan = bulanan('kawalan','14'); # papar bulan dlm tahun semasa
 		$cari[] = array('fix'=>'x=','atau'=>'WHERE','medan'=>'fe','apa'=>$fe);
@@ -47,7 +89,7 @@ class Paparan extends Kawal
             $medanRangka = $this->medanRangka;
 			$medanData = $this->medanData;
             $sv = $this->sv;
-		/*
+		
         # mula papar semua dalam $myTable
         foreach ($bulanan as $key => $myTable)
         {# mula ulang table
@@ -67,7 +109,7 @@ class Paparan extends Kawal
         }# tamat ulang table
         
         # semak pembolehubah $this->papar->cariApa
-        echo '<pre>', print_r($this->papar->cariApa, 1) . '</pre><br>';
+        //echo '<pre>', print_r($this->papar->cariApa, 1) . '</pre><br>';
 		
         # Set pemboleubah utama
         $this->papar->pegawai = senarai_kakitangan();
@@ -81,8 +123,6 @@ class Paparan extends Kawal
 		else //echo 'ubah';
 			$this->papar->baca('kawalan/index', 0);
 		//*/
-
-		
     }
 
     public function utama($item = 30, $ms = 1, $utama = null, $respon = null, $fe = null) 
